8cdbfd2157bbe3b6d22aaf313fdb86b65b236486
DigitalMarketingHubBD <mahmudulkabir243@gmail.com>
2026-04-17 00:38:28 +0600

    Update shopify.server.js

--- shopify.server.js
+++ shopify.server.js
@@ -1,1 +1,20 @@
+// app/shopify.server.js
+import { shopifyApp } from "@shopify/app";
+import { restResources } from "@shopify/shopify-api/rest/admin/2025-01";
+
+const shopify = shopifyApp({
+  api: {
+    apiKey: process.env.SHOPIFY_API_KEY,
+    apiSecretKey: process.env.SHOPIFY_API_SECRET,
+    scopes: ["read_products", "write_products", "read_content", "write_content", "read_themes"],
+    hostName: process.env.HOST.replace(/https?:\/\//, ""),
+    restResources,
+  },
+  auth: {
+    path: "/auth",
+    callbackPath: "/auth/callback",
+  },
+});
+
+export default shopify;
 
